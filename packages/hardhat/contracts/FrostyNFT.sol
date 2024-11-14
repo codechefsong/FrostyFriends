9a82455ccfa8e97db73b53aa6cb59e590583a903
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-11-14 14:36:09 -0500

    add ERC721 NFT

---
+++ packages/hardhat/contracts/FrostyNFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract FrostyNFT is ERC721URIStorage {
+  uint256 public numberOfNFT = 0;
+
+  constructor() ERC721("Frosty NFT", "FNFT") {}
+
+  function mintFrosty(address _to, string memory _tokenURI_) public {
+    _mint(_to, numberOfNFT);
+    _setTokenURI(numberOfNFT, _tokenURI_);
+    numberOfNFT++;
+  }
+}
